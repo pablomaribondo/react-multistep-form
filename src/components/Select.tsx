83513eb3cf4ee549cf8b4078cb819c553af3073a
Pablo Maribondo <pablomgvieira@gmail.com>
2021-07-11 20:48:14 -0300

    feat: create Select component

---
+++ src/components/Select.tsx
@@ -0,0 +1,35 @@
+import { FC, FormEvent } from 'react';
+
+type Option = {
+  value: string;
+  label: string;
+};
+
+interface SelectProps extends HTMLSelectElement {
+  onChange: (event: FormEvent<HTMLSelectElement>) => void;
+  choices: Option[];
+  error: string;
+}
+
+const Select: FC<SelectProps> = ({ name, value, onChange, choices, error }) => {
+  return (
+    <div className="mb-5">
+      <div
+        className={
+          error ? 'select is-fullwidth is-danger' : 'select is-fullwidth'
+        }
+      >
+        <select name={name} value={value} onChange={onChange}>
+          {choices.map(choice => (
+            <option key={choice.value} value={choice.value}>
+              {choice.label}
+            </option>
+          ))}
+        </select>
+      </div>
+      {error && <div className="has-text-danger-dark">{error}</div>}
+    </div>
+  );
+};
+
+export default Select;
