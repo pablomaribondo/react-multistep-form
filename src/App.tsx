ed8cc925d3a90739b2f3cca20fbe7928bbbbd9f3
Pablo Maribondo <pablomgvieira@gmail.com>
2021-07-11 20:28:56 -0300

    feat: add basic layout to app component

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,24 @@ import './App.css';
 
 const App: FC = () => {
   return (
-    <div>
-      <p>React Multi Step Form</p>
-    </div>
+    <>
+      <section className="hero is-light has-text-centered">
+        <div className="hero-body">
+          <div className="container">
+            <h1 className="is-size-1">
+              ReactJS multi step form with validation
+            </h1>
+          </div>
+        </div>
+      </section>
+      <div className="container pt-5">
+        <div className="columns">
+          <div className="column is-half is-offset-one-quarter">
+            {/* form component */}
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
